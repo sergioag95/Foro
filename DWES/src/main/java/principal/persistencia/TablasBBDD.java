8fb918ed68f0453ceaf1892826ef78960416eefb
Sergio Alonso <sergioagov8j@gmail.com>
2023-04-25 14:06:19 +0200

    problemas al crear arraylist de hilos

--- DWES/src/main/java/principal/persistencia/TablasBBDD.java
+++ DWES/src/main/java/principal/persistencia/TablasBBDD.java
@@ -1,5 +1,8 @@
 package principal.persistencia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import principal.modelo.Categoria;
 import principal.modelo.Hilo;
 import principal.modelo.Post;
@@ -10,12 +13,14 @@ public class TablasBBDD {
 	public void crearTablas() {
 		
 		Usuario u1 = new Usuario();
+		
 		u1.setUsername("sergioag95");
 		u1.setNombre("Sergio");
 		
+		//List hilosu1 = new ArrayList <Hilo>();
+		
 		
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		usuarioDAO.insertarUsuarioJPA(u1);
 		
 		
 		Categoria c1 = new Categoria();
@@ -24,17 +29,21 @@ public class TablasBBDD {
 	
 		CategoriaDAO categoriaDAO = new CategoriaDAO();
 		
-		categoriaDAO.insertarCategoriaJPA(c1);
 		
-		categoriaDAO.listarCategoriasJPA();
+		
 		
 		Hilo h1 = new Hilo();
+		
+		
 		h1.setTitulo("Baloncesto");
 		h1.setUsuario(u1);
 		h1.setCategoria(c1);
+		h1.setUsuario(u1);
+		//hilosu1.add(h1);
+		//u1.setHilos(hilosu1);
+		
 		
 		HiloDAO hiloDAO = new HiloDAO();
-		hiloDAO.insertarBocadilloJPA(h1);
 		
 		Post p1 = new Post();
 		
@@ -43,7 +52,14 @@ public class TablasBBDD {
 		p1.setContenido("Texto Ejemplo");
 		
 		PostDAO postDAO = new PostDAO();
+		
+		
+		usuarioDAO.insertarUsuarioJPA(u1);
+		categoriaDAO.insertarCategoriaJPA(c1);
+		hiloDAO.insertarBocadilloJPA(h1);
 		postDAO.insertarPostJPA(p1);
+
+
 		
 		
 		
